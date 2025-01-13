f84d76fd7ab433f47ad6a921f32b9488044d14dc
minnc2501 <minz2501z@gmail.com>
2025-01-13 18:25:38 +0700

    basic setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,14 @@
-"use client";
+import ThemeToggle from "@/components/shared/theme-toggle";
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <div className="font-bold text-red-300">Welcome to this courses</div>;
+  return (
+    <div className="p-20">
+      <div>
+        <ThemeToggle />
+      </div>
+      <div className="text-red-300 font-barlow">Welcome to this courses</div>
+      <Button variant="link">Click here</Button>
+    </div>
+  );
 }
